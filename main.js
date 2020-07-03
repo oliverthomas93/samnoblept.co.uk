fd113888baf2c002e75f9d53374638ae058dc7c4
Oliver Thomas <63861649+oliverthomas93@users.noreply.github.com>
2020-07-03 19:20:50 +0100

    JS
    Script to make the nav bar more mobile friendly.

---
+++ main.js
@@ -0,0 +1,38 @@
+let menu = document.querySelector('#menu');
+let nav = document.querySelector('nav');
+let hidden = document.querySelector('#hidden-nav');
+let link1 = document.querySelector('#link1');
+let link2 = document.querySelector('#link2');
+let link3 = document.querySelector('#link3');
+let link4 = document.querySelector('#link4');
+
+let topNav = () => {
+    if (nav.style.background === "black") {
+        nav.style.background = "linear-gradient(black, transparent)";
+    } else {
+        nav.style.background = "black";
+}
+    if (hidden.style.display === "block") {
+        hidden.style.display = "none";
+    } else {
+        hidden.style.display = "block";
+    }
+}
+let goAway = () => {
+    if (nav.style.background === "black") {
+        nav.style.background = "linear-gradient(black, transparent)";
+    } else {
+        nav.style.background = "black";
+}
+    if (hidden.style.display === "block") {
+        hidden.style.display = "none";
+    } else {
+        hidden.style.display = "block";
+    }
+}
+
+menu.addEventListener('click', topNav);
+link1.addEventListener('click', goAway);
+link2.addEventListener('click', goAway);
+link3.addEventListener('click', goAway);
+link4.addEventListener('click', goAway);
